7e65cb7915c6532ec8f74da784bb029a3f38fdd9
km4president22 <104132752+km4president22@users.noreply.github.com>
2022-04-21 09:47:13 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+var http = require('http');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write('Hello World!');
+  res.end();
+}).listen(8080);
+
+console.log("Open http://localhost:8080 in a browser to see your website!")
